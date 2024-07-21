ef3e9c435c6aad6e1727c3c9ce804b81fd8196c4
mateodonino <mateodonino@gmail.com>
2024-07-21 19:45:32 -0300

    v2.5

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -16,7 +16,7 @@ export default function Experience() {
         {" "}
         <div className="max-w-7xl mx-auto px-8">
           <Title
-            text={"Experience 💼"}
+            text={"Experienc 💼"}
             className="flex flex-col items-center justify-center rotate-6"
           />
         </div>
